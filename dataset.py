3183618aae6b44820b94f527b140cd58234c6419
refor53 <alseo784@gmail.com>
2024-10-31 23:08:13 +0900

    Exp: #10, dataset.py에 1~3번까지의 과정 추가

--- dataset.py
+++ dataset.py
@@ -335,62 +335,157 @@ def filter_vertices(vertices, labels, ignore_under=0, drop_under=0):
     return new_vertices, new_labels
 
 
-class SceneTextDataset(Dataset):
-    def __init__(self, root_dir,
-                 split='train',
-                 image_size=2048,
-                 crop_size=1024,
-                 ignore_under_threshold=10,
-                 drop_under_threshold=1,
-                 color_jitter=True,
-                 normalize=True):
-        self._lang_list = ['chinese', 'japanese', 'thai', 'vietnamese']
+# class SceneTextDataset(Dataset):
+#     def __init__(self, root_dir,
+#                  split='train',
+#                  image_size=2048,
+#                  crop_size=1024,
+#                  ignore_under_threshold=10,
+#                  drop_under_threshold=1,
+#                  color_jitter=True,
+#                  normalize=True):
+#         self._lang_list = ['chinese', 'japanese', 'thai', 'vietnamese']
+#         self.root_dir = root_dir
+#         self.split = split
+#         total_anno = dict(images=dict())
+#         for nation in self._lang_list:
+#             with open(osp.join(root_dir, '{}_receipt/ufo/{}.json'.format(nation, split)), 'r', encoding='utf-8') as f:
+#                 anno = json.load(f)
+#             for im in anno['images']:
+#                 total_anno['images'][im] = anno['images'][im]
+
+#         self.anno = total_anno
+#         self.image_fnames = sorted(self.anno['images'].keys())
+
+#         self.image_size, self.crop_size = image_size, crop_size
+#         self.color_jitter, self.normalize = color_jitter, normalize
+
+#         self.drop_under_threshold = drop_under_threshold
+#         self.ignore_under_threshold = ignore_under_threshold
+
+# merged_receipts 폴더 구조에 맞게 수정된 코드
+def lsa_processing(img_array):
+    """L*a*b* 색공간에서 L채널 처리"""
+    # 이미지가 그레이스케일인 경우 바로 처리
+    if len(img_array.shape) == 2:
+        l = img_array
+    else:
+        # RGB to LAB 변환
+        lab = cv2.cvtColor(img_array, cv2.COLOR_RGB2LAB)
+        l, a, b = cv2.split(lab)
+    
+    # L채널에 대해 CLAHE 적용
+    clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
+    enhanced_l = clahe.apply(l)
+    
+    # Bilateral 필터로 노이즈 제거하면서 경계는 보존
+    smooth_l = cv2.bilateralFilter(enhanced_l, 9, 75, 75)
+
+    return smooth_l
+
+def preprocess_receipt(image):
+    """영수증 이미지 전처리 함수"""
+    # PIL Image를 numpy array로 변환
+    if isinstance(image, Image.Image):
+        # RGB로 변환 보장
+        image = image.convert('RGB')
+        img_array = np.array(image)
+    else:
+        img_array = image.copy()
+        # 그레이스케일 이미지인 경우 RGB로 변환
+        if len(img_array.shape) == 2:
+            img_array = cv2.cvtColor(img_array, cv2.COLOR_GRAY2RGB)
+    
+    smooth_l = lsa_processing(img_array)
+
+    # 적응형 이진화 적용
+    binary = cv2.adaptiveThreshold(
+        smooth_l, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+        cv2.THRESH_BINARY, 11, 2
+    )
+    
+    # 노이즈 제거
+    denoised = cv2.medianBlur(binary, 3)
+
+    # 대비 향상
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    enhanced = clahe.apply(denoised)
+
+    return Image.fromarray(enhanced)
+
+class SceneTextDataset:
+    def __init__(self, root_dir, split='train', image_size=2048, crop_size=1024, 
+                 ignore_under_threshold=10, drop_under_threshold=1,
+                 color_jitter=True, normalize=True):
         self.root_dir = root_dir
         self.split = split
-        total_anno = dict(images=dict())
-        for nation in self._lang_list:
-            with open(osp.join(root_dir, '{}_receipt/ufo/{}.json'.format(nation, split)), 'r', encoding='utf-8') as f:
-                anno = json.load(f)
-            for im in anno['images']:
-                total_anno['images'][im] = anno['images'][im]
-
-        self.anno = total_anno
-        self.image_fnames = sorted(self.anno['images'].keys())
-
-        self.image_size, self.crop_size = image_size, crop_size
-        self.color_jitter, self.normalize = color_jitter, normalize
-
-        self.drop_under_threshold = drop_under_threshold
+        self.image_size = image_size
+        self.crop_size = crop_size
+        self.color_jitter = color_jitter
+        self.normalize = normalize
         self.ignore_under_threshold = ignore_under_threshold
+        self.drop_under_threshold = drop_under_threshold
 
-    def _infer_dir(self, fname):
-        lang_indicator = fname.split('.')[1]
-        if lang_indicator == 'zh':
-            lang = 'chinese'
-        elif lang_indicator == 'ja':
-            lang = 'japanese'
-        elif lang_indicator == 'th':
-            lang = 'thai'
-        elif lang_indicator == 'vi':
-            lang = 'vietnamese'
-        else:
-            raise ValueError
-        return osp.join(self.root_dir, f'{lang}_receipt', 'img', self.split)
+        # merged_receipts 폴더 구조에 맞게 수정
+        self.image_dir = osp.join(root_dir, 'images', split)
+        self.annotation_path = osp.join(root_dir, f'{split}.json')
+        
+        if not osp.exists(self.image_dir):
+            raise ValueError(f"Image directory not found: {self.image_dir}")
+        if not osp.exists(self.annotation_path):
+            raise ValueError(f"Annotation file not found: {self.annotation_path}")
+            
+        self.annotations = []
+        
+        # UFO 형식 데이터 로드
+        with open(self.annotation_path, 'r', encoding='utf-8') as f:
+            annotations = json.load(f)
+        
+        for image_name, annotation in annotations['images'].items():
+            img_path = osp.join(self.image_dir, image_name)
+            if osp.exists(img_path):  # 이미지 파일이 실제로 존재하는지 확인
+                self.annotations.append({
+                    'file_name': image_name,
+                    'words': annotation.get('words', dict()),
+                    'img_path': img_path
+                })
+            else:
+                print(f"Warning: Image not found: {img_path}")
+
+
+    # def _infer_dir(self, fname):
+    #     lang_indicator = fname.split('.')[1]
+    #     if lang_indicator == 'zh':
+    #         lang = 'chinese'
+    #     elif lang_indicator == 'ja':
+    #         lang = 'japanese'
+    #     elif lang_indicator == 'th':
+    #         lang = 'thai'
+    #     elif lang_indicator == 'vi':
+    #         lang = 'vietnamese'
+    #     else:
+    #         raise ValueError
+    #     return osp.join(self.root_dir, f'{lang}_receipt', 'img', self.split)
+
     def __len__(self):
-        return len(self.image_fnames)
+        # image_fnames 대신 annotations 사용
+        return len(self.annotations)
 
     def __getitem__(self, idx):
-        image_fname = self.image_fnames[idx]
-        image_fpath = osp.join(self._infer_dir(image_fname), image_fname)
-
+        # image_fname 대신 annotation 사용
+        annotation = self.annotations[idx]
+        image_fpath = annotation['img_path']
+        
         vertices, labels = [], []
-        for word_info in self.anno['images'][image_fname]['words'].values():
-            num_pts = np.array(word_info['points']).shape[0]
-            if num_pts > 4:
+        for word_info in annotation['words'].values():
+            points = np.array(word_info['points'])
+            if points.shape[0] > 4:
                 continue
-            vertices.append(np.array(word_info['points']).flatten())
+            vertices.append(points.flatten())
             labels.append(1)
-        vertices, labels = np.array(vertices, dtype=np.float32), np.array(labels, dtype=np.int64)
+        vertices = np.array(vertices, dtype=np.float32)
+        labels = np.array(labels, dtype=np.int64)
+
 
         vertices, labels = filter_vertices(
             vertices,
@@ -400,6 +495,8 @@ class SceneTextDataset(Dataset):
         )
 
         image = Image.open(image_fpath)
+        # 전처리 적용
+        image = preprocess_receipt(image)
         image, vertices = resize_img(image, vertices, self.image_size)
         image, vertices = adjust_height(image, vertices)
         image, vertices = rotate_img(image, vertices)
